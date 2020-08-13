3b529fc5b01f3d5d45c0367dc32af675f7c7e56c
Tyler Simeone <tyler.simeone@gmail.com>
2020-08-13 10:44:20 -0500

    updated admin app's urls.py to include necessary modules for routes and paths

--- mtbconnect/mtbconnect/urls.py
+++ mtbconnect/mtbconnect/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 from django.contrib import admin
 from django.urls import path
 
+router = routers.DefaultRouter(trailing_slash=False)
+# Register your viewset routes here
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # add paths to all routes, as well as auth viewsets,
+    # obtain_auth_token, and the last api-auth one as well..
 ]
