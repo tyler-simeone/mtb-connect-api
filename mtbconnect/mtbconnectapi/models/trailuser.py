45cef69187dc04fa3040de556b9c3ff5453bd468
Tyler Simeone <tyler.simeone@gmail.com>
2020-08-13 11:15:21 -0500

    Migrated user and trailuser models, adding trail model now

--- mtbconnect/mtbconnectapi/models/trailuser.py
+++ mtbconnect/mtbconnectapi/models/trailuser.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from .user import User
-from .trail import Trail
+# from .trail import Trail
 
 class TrailUser(models.Model):
     
