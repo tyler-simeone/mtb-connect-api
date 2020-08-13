e21fc15d2e79b061121be3d92675d3171a795b0f
Tyler Simeone <tyler.simeone@gmail.com>
2020-08-13 11:09:44 -0500

    added TrailUser model

--- mtbconnect/mtbconnectapi/models/trailuser.py
+++ mtbconnect/mtbconnectapi/models/trailuser.py
@@ -1,18 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from .user import User
+from .trail import Trail
 
-class User(models.Model):
+class TrailUser(models.Model):
     
     userId = models.ForeignKey(User, on_delete = models.CASCADE)
-    trailId = models.ForeignKey(User, on_delete = models.CASCADE)
+    trailId = models.ForeignKey(Trail, on_delete = models.CASCADE)
     
     class Meta:
         verbose_name = ("TrailUser")
         verbose_name_plural = ("TrailUsers")        
         
     def __str__(self):
-        return f"User ID: {self.user}"
+        return f"User ID: {self.userId} Trail: {self.trailId}"
     
     def get_absolute_url(self):
-        return reverse("user_detail", kwargs={"pk": self.pk})
+        return reverse("trail_user_detail", kwargs={"pk": self.pk})
