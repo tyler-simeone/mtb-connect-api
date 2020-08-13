466182855af84006a1d789568e1ba2c015ba64e9
Tyler Simeone <tyler.simeone@gmail.com>
2020-08-13 11:11:05 -0500

    commented out inexisting modules in the models package for now

--- mtbconnect/mtbconnectapi/models/trailuser.py
+++ mtbconnect/mtbconnectapi/models/trailuser.py
@@ -7,14 +7,14 @@ from .trail import Trail
 class TrailUser(models.Model):
     
     userId = models.ForeignKey(User, on_delete = models.CASCADE)
-    trailId = models.ForeignKey(Trail, on_delete = models.CASCADE)
+    # trailId = models.ForeignKey(Trail, on_delete = models.CASCADE)
     
     class Meta:
         verbose_name = ("TrailUser")
         verbose_name_plural = ("TrailUsers")        
         
     def __str__(self):
-        return f"User ID: {self.userId} Trail: {self.trailId}"
+        return f"User ID: {self.userId}"
     
     def get_absolute_url(self):
         return reverse("trail_user_detail", kwargs={"pk": self.pk})
