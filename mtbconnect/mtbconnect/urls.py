15d8f5ea5158e6b0f703abc86a8f8ecdf3ae429a
Tyler Simeone <tyler.simeone@gmail.com>
2020-08-14 10:46:12 -0500

    registered existing trail viewset as a route in admin apps urls.py

--- mtbconnect/mtbconnect/urls.py
+++ mtbconnect/mtbconnect/urls.py
@@ -22,10 +22,11 @@ from mtbconnectapi.views import *
 
 router = routers.DefaultRouter(trailing_slash=False)
 # Register your viewset routes here
+router.register(r'trails', Trails, 'trail')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include(router.urls)),
+    path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
     # add paths to all routes, as well as auth viewsets,
