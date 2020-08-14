42a02dc6c93cb3502ab7dfbd5515bffbd14b3938
Tyler Simeone <tyler.simeone@gmail.com>
2020-08-14 11:28:21 -0500

    delete method added to trail viewset

--- mtbconnect/mtbconnectapi/views/trail.py
+++ mtbconnect/mtbconnectapi/views/trail.py
@@ -64,6 +64,23 @@ class Trails(ViewSet):
 
         return Response({}, status=status.HTTP_204_NO_CONTENT)
 
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single trail
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            trail = Trail.objects.get(pk=pk)
+            trail.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Trail.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
     def list(self, request):
         
         trails = Trail.objects.all()
