17407a5ee17260d801232fffe64454475c015db7
Tyler Simeone <tyler.simeone@gmail.com>
2020-10-01 16:57:56 -0500

    updated CORS whitelist to include newly-hosted MTB Connect React Front-end from AWS s3 endpoint

--- mtbconnect/mtbconnect/settings.py
+++ mtbconnect/mtbconnect/settings.py
@@ -68,7 +68,9 @@ MIDDLEWARE = [
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8080',
     'http://localhost:3000',
-    'http://127.0.0.1:8080'
+    'http://127.0.0.1:8080',
+    'http://127.0.0.1:8000'
+    'http://mtbconnect.s3-website.us-east-2.amazonaws.com',
 )
 
 ROOT_URLCONF = 'mtbconnect.urls'
