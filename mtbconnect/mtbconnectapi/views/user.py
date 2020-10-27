24258997d1b93e94beaf29ce982d86fb1debc91b
Tyler Simeone <tyler.simeone@gmail.com>
2020-10-27 13:25:52 -0500

    PUT method for user profile now working

--- mtbconnect/mtbconnectapi/views/user.py
+++ mtbconnect/mtbconnectapi/views/user.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponseServerError
+from django.contrib.auth.models import User as AuthUser
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
@@ -31,6 +32,26 @@ class Users(ViewSet):
 
         except Exception as ex:
             return HttpResponseServerError(ex)
+
+    def put(self, request, pk=None):
+        """Handle PUT requests for an individual user
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        user = User.objects.get(user_id=pk)
+        auth_user = AuthUser.objects.get(pk=pk)
+
+        auth_user.first_name = request.data["first_name"]
+        auth_user.last_name = request.data["last_name"]
+        user.avatar_img = request.data["avatar_img"]
+        auth_user.username = request.data["username"]
+        auth_user.email = request.data["email"]
+        
+
+        user.save()
+        auth_user.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
    
     def list(self, request):
 
